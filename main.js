e2082347a294cc73034d19650e37902f92a94b1d
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-04-03 17:02:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// main.js
+
+const panoramaImage = new PANOLENS.ImagePanorama("images/image3.jpeg");
+const panoramaImage2 = new PANOLENS.ImagePanorama("images/image1.jpeg");
+
+const imageContainer = document.querySelector(".image-container");
+
+const btn = document.querySelector("#btn").addEventListener("click", yesRoom);
+const btn2 = document.querySelector("#btn2").addEventListener("click", noRoom);
+
+const viewer = new PANOLENS.Viewer({
+  container: imageContainer,
+  autoRotate: true,
+  autoRotateSpeed: 0.6,
+  controlBar: true,
+});
+function yesRoom() {
+  viewer.add(panoramaImage);
+}
+function noRoom() {
+  // viewer.remove(panoramaImage2);
+  // viewer.remove(panoramaImage2);
+  viewer.remove(panoramaImage);
+}
+// viewer.add(panoramaImage);
